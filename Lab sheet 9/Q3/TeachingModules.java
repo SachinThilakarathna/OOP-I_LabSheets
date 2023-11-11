4a4305b7c75b592a357ebe9241b534bac8dd1794
Sachin Thilakarathna <145207063+SachinThilakarathna@users.noreply.github.com>
2023-11-11 08:20:24 +0400

    Update TeachingModules.java

--- Lab sheet 9/Q3/TeachingModules.java
+++ Lab sheet 9/Q3/TeachingModules.java
@@ -1,9 +1,9 @@
 public class TeachingModules {
     public static void main(String args[]){
-
+        Departments c=new Departments();
         Departments a=new ComputerScience();
         Departments b=new ComputerEngineering();
-
+        c.display();
         a.display();
         b.display();      
     }
